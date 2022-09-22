8ec8dd669d5554f7b100cc31a1627cc08d955852
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-09-22 13:37:53 +0400

    1

---
+++ src/main/java/com/examplesermapss/setmaps/SetMapsApplication.java
@@ -0,0 +1,13 @@
+package com.examplesermapss.setmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetMapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetMapsApplication.class, args);
+    }
+
+}
